b81ca43d43d52a822d1435ff7dba338a48beb13b
vinay krishna <vinkrish@vinays-MacBook-Air.local>
2019-01-23 23:14:15 +0530

    updated work experience and education

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,95 +20,99 @@ class ResumeIndex extends React.Component {
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
         <Bio />
+        <h3>Work Experience</h3>
         <VerticalTimeline layout="1-column">
-        <VerticalTimelineElement
+          <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="July, 2018 - Dec, 2018"
+            date="July 2018 - Dec 2018"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faCode} />}
-        >
+          >
             <h3 className="vertical-timeline-element-title">Tech Lead</h3>
             <h4 className="vertical-timeline-element-subtitle">Think And Learn Pvt Ltd</h4>
             <p>
-            Creative Direction, User Experience, Visual Design, Project Management, Team Leading
+              Tech Stack: NodeJS, React, AngularJS, MongoDB
             </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2010 - 2011"
+            date="Jan 2017 - Jun 2018"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faCode} />}
-        >
-            <h3 className="vertical-timeline-element-title">Art Director</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          >
+            <h3 className="vertical-timeline-element-title">Founder</h3>
+            <h4 className="vertical-timeline-element-subtitle">Shikshitha</h4>
             <p>
-            Creative Direction, User Experience, Visual Design, SEO, Online Marketing
+              Tech Stack: Android, Jersey RESTful API, Angular 2+, AWS, MYSQL
             </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2008 - 2010"
+            date="Dec 2013 - May 2016"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faCode} />}
-        >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">Los Angeles, CA</h4>
+          >
+            <h3 className="vertical-timeline-element-title">Android App Developer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Banyan Learning Solutions (SchoolCom)</h4>
             <p>
-            User Experience, Visual Design
+              
             </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
             className="vertical-timeline-element--work"
-            date="2006 - 2008"
+            date="Dec 2017 - Jan 2018 / Oct 2016 - Jan 2017 / 2015"
             iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faCode} />}
-        >
-            <h3 className="vertical-timeline-element-title">Web Designer</h3>
-            <h4 className="vertical-timeline-element-subtitle">San Francisco, CA</h4>
+          >
+            <h3 className="vertical-timeline-element-title">Freelancer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Upright / Study Manager / FabFresh</h4>
             <p>
-            User Experience, Visual Design
+              Tech Stack: ASP.NET / Android App Development++
             </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+        <h3>Education</h3>
+        <VerticalTimeline layout="1-column">
+          <VerticalTimelineElement
             className="vertical-timeline-element--education"
-            date="April 2013"
+            date="2011 - 2013"
             iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faCode} />}
-        >
-            <h3 className="vertical-timeline-element-title">Content Marketing for Web, Mobile and Social Media</h3>
-            <h4 className="vertical-timeline-element-subtitle">Online Course</h4>
+          >
+            <h3 className="vertical-timeline-element-title">Master of Technology</h3>
+            <h4 className="vertical-timeline-element-subtitle">Computer Science, SIT, Tumkur</h4>
             <p>
-            Strategy, Social Media
+              CGPA: 7.93
             </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
             className="vertical-timeline-element--education"
-            date="November 2012"
+            date="2007 - 2011"
             iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faCode} />}
-        >
-            <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
-            <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+          >
+            <h3 className="vertical-timeline-element-title">Bachelor of Engineering</h3>
+            <h4 className="vertical-timeline-element-subtitle">Electronics and Communication, SSIT, Tumkur</h4>
             <p>
-            Creative Direction, User Experience, Visual Design
+              CGPA: 8.67
             </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
             className="vertical-timeline-element--education"
-            date="2002 - 2006"
+            date="2005 - 2007"
             iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faCode} />}
-        >
-            <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-            <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+          >
+            <h3 className="vertical-timeline-element-title">Class 12</h3>
+            <h4 className="vertical-timeline-element-subtitle">Sapthagiri Pre-University College, Tumkur</h4>
             <p>
-            Creative Direction, Visual Design
+              84%
             </p>
-        </VerticalTimelineElement>
-        <VerticalTimelineElement
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
             iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
             icon={<FontAwesomeIcon icon={faCode} />}
-        />
+          />
         </VerticalTimeline>
       </Layout>
     )
