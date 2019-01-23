56ebd563374f0b41d5aac71d13c1c8d2c83d22af
vinay krishna <vinkrish@vinays-MacBook-Air.local>
2019-01-23 15:11:23 +0530

    added public folder

---
+++ public/component---src-pages-index-js-844496b4d66adc632393.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{133:function(n,e,o){"use strict";o.r(e);var t=o(0),l=o.n(t);e.default=function(){return l.a.createElement("div",null,"Hello world!")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-844496b4d66adc632393.js.map
