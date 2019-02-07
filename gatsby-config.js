f3b7f2a5489983fecd2cf9ead553b3a5c34cbb77
Vinay Krishna <vinaykrishna1989@gmail.com>
2019-02-07 15:03:51 +0530

    fixed duplicate config entry

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,19 @@ module.exports = {
             },
           },
           {
+            resolve: "gatsby-remark-embed-gist",
+            options: {
+              // Optional:
+    
+              // the github handler whose gists are to be accessed
+              username: 'vinkrish',
+    
+              // a flag indicating whether the github default gist css should be included or not
+              // default: true
+              includeDefaultCss: true
+            }
+          },
+          {
             resolve: `gatsby-remark-prismjs`,
             options: {
               classPrefix: "language-",
@@ -84,26 +97,6 @@ module.exports = {
       options: {
         pathToConfigModule: `src/utils/typography`,
       },
-    },
-    {
-      resolve: "gatsby-transformer-remark",
-      options: {
-        plugins: [
-          {
-            resolve: "gatsby-remark-embed-gist",
-            options: {
-              // Optional:
-    
-              // the github handler whose gists are to be accessed
-              username: 'weirdpattern',
-    
-              // a flag indicating whether the github default gist css should be included or not
-              // default: true
-              includeDefaultCss: true
-            }
-          }
-        ]
-      }
     },
   ],
 }
