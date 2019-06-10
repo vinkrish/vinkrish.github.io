fd02b72673e3db2d58726700752646395ef6b59a
Vinay Krishna <vinaykrishna1989@gmail.com>
2019-06-10 12:04:25 +0530

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8637c0613931e4d0f448.js"
+    "url": "webpack-runtime-72c3bec490020c681b9d.js"
   },
   {
     "url": "app.1e4ea78f50937110b9d4.css"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "4b07550d958cc4d2cfcb5992acf02001"
+    "revision": "d61eff1afb0500f76ac47e42597bc51f"
   },
   {
     "url": "component---src-pages-404-js-0ac185ce4288e04d0854.js"
